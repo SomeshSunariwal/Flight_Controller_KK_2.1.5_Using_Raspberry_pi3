ebbb868d2d131042d4bfd5ee4d396105dc8a1461
Somesh Sunariwal <38235467+SomeshSunariwal@users.noreply.github.com>
2019-05-13 15:34:12 +0530

    Update Drone.py

--- Drone.py
+++ Drone.py
@@ -15,14 +15,17 @@ class Drone:
         self.rudd = 21
 
         # Max Min Value
-        self.thr_max = 2350
+        self.thr_max = 2300
         self.thr_min = 1100
-        self.rudd_max = 2000
-        self.rudd_min = 1000
-        self.ail_max = 2000
-        self.ail_min = 1000
-        self.ele_max = 2000
-        self.ele_min = 1000
+        self.rudd_max = 1900
+        self.rudd_mid = 1500
+        self.rudd_min = 1100
+        self.ail_max = 1900
+        self.ail_mid = 1500
+        self.ail_min = 1100
+        self.ele_max = 1900
+        self.ele_mid = 1500
+        self.ele_min = 1100
 
         ###  Initial Value
         self.thr_value = 1100
@@ -58,43 +61,43 @@ class Drone:
             self.thr_value += 10
             self.pi.set_servo_pulsewidth(self.thr, self.thr_value)  # incrementing the speed like hell
             if self.thr_value >= self.thr_max:
-                self.thr_value = 2350
+                self.thr_value = self.thr_max
             print("th speed = %d" % self.thr_value)
 
         elif inp == "s":
             self.thr_value -= 10
-            self.pi.set_servo_pulsewidth(self.thr, self.thr_value)  # incrementing the speed like hell
+            self.pi.set_servo_pulsewidth(self.thr, self.thr_value)
             if self.thr_value <= self.thr_min:
-                self.thr_value = 1100
+                self.thr_value = self.thr_min
             print("th speed = %d" % self.thr_value)
 
         # Rudder Setup
-        elif inp == 'q':
+        elif inp == 'e':
             self.rudd_value += 10
             self.pi.set_servo_pulsewidth(self.rudd, self.rudd_value)
             print("Rudd_speed = %d" % self.rudd_value)
             if self.rudd_value >= self.rudd_max:
-                self.rudd_value = 2000
-        elif inp == 'e':
+                self.rudd_value = self.rudd_max
+        elif inp == 'q':
             self.rudd_value -= 10
             print("Rudd_speed = %d" % self.rudd_value)
             self.pi.set_servo_pulsewidth(self.rudd, self.rudd_value)
             if self.rudd_value <= self.rudd_min:
-                self.rudd_value = 1000
+                self.rudd_value = self.rudd_min
 
         # Ailereon Setup (Left And Right)
         elif inp == 'a':
-            self.ail_value += 10
-            self.pi.set_servo_pulsewidth(self.ail, self.ail_value)
-            print("Ail_speed = %d" % self.ail_value)
-            if self.ail_value >= self.ail_max:
-                self.rudd_value = 2000
-        elif inp == 'd':
             self.ail_value -= 10
+            self.pi.set_servo_pulsewidth(self.ail, self.ail_value)
+            print("Ail_speed = %d" % self.ail_value)
+            if self.ail_value <= self.ail_min:
+                self.ail_value = self.ail_min
+        elif inp == 'd':
+            self.ail_value += 10
             print("Ail_speed = %d" % self.ail_value)
             self.pi.set_servo_pulsewidth(self.ail, self.ail_value)
-            if self.ail_value <= self.ail_min:
-                self.ail_value = 1000
+            if self.ail_value >= self.ail_max:
+                self.ail_value = self.ail_max
 
         # Ele Setup (Front and Back)
         elif inp == 'r':
@@ -102,13 +105,13 @@ class Drone:
             self.pi.set_servo_pulsewidth(self.ele, self.ele_value)
             print("Ele_speed = %d" % self.ele_value)
             if self.ele_value >= self.ele_max:
-                self.ele_value = 2000
+                self.ele_value = self.ele_max
         elif inp == 'f':
             self.ele_value -= 10
             print("Ele_speed = %d" % self.ele_value)
             self.pi.set_servo_pulsewidth(self.ele, self.ele_value)
             if self.ele_value <= self.ele_min:
-                self.ele_value = 1000
+                self.ele_value = self.ele_min
 
         # Arm Drone
         elif inp == "z":
@@ -127,12 +130,43 @@ class Drone:
             print("Drone Disarm")
 
         # Stop
-        elif inp == 'v':
+        elif inp == 'c':
             print("Stop")
-            self.pi.set_servo_pulsewidth(self.thr, self.thr_min)
+            self.thr_value = self.thr_min
+            self.rudd_value = self.rudd_mid
+            self.ail_value = self.ail_mid
+            self.ele_value = self.ele_mid
+            self.pi.set_servo_pulsewidth(self.thr, self.thr_value)
             self.pi.set_servo_pulsewidth(self.rudd, self.rudd_value)
             self.pi.set_servo_pulsewidth(self.ail, self.ail_value)
             self.pi.set_servo_pulsewidth(self.ele, self.ele_value)
+
+        # Safe mode
+        elif inp == 'v':
+            print("Safe Mode")
+            self.pi.set_servo_pulsewidth(self.rudd, 1720)
+            self.pi.set_servo_pulsewidth(self.ail, 1900)
+            time.sleep(2)
+            self.pi.set_servo_pulsewidth(self.ail, 1500)
+            self.pi.set_servo_pulsewidth(self.rudd, 1500)
+            print("Safe Mode")
+
+        # Landing Drone
+        elif inp == "l":
+            print("Landing")
+            self.rudd_value = self.rudd_mid
+            self.ail_value = self.ail_mid
+            self.ele_value = self.ele_mid
+            self.pi.set_servo_pulsewidth(self.rudd, self.rudd_value)
+            self.pi.set_servo_pulsewidth(self.ail, self.ail_value)
+            self.pi.set_servo_pulsewidth(self.ele, self.ele_value)
+            value = self.thr_value - self.thr_min
+            for i in range (value, 0, -10):
+                i = i + self.thr_min
+                self.pi.set_servo_pulsewidth(self.thr, i)
+                time.sleep(.2)
+            self.thr_value = self.thr_min
+            print("Landing done")
 
         else:
             self.pi.stop()
